484a371398944c7a5c1d3f6dff37fa4d54a4568d
AJIyanu <aderemijoseph@live.com>
2023-01-02 11:08:05 +0100

    first attempt yet to be reviewed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,3 +2,22 @@
 """interview question module
 locked boxes
 """
+
+
+def canUnlockAll(boxes):
+    """check if all boxes can be unlocked"""
+    valid = []
+    for n in range(len(boxes)):
+        valid.append("locked")
+    valid[0] = "unlocked"
+    for i in range(len(valid)):
+        for check in range(len(valid)):
+            if valid[check] == "unlocked":
+                getkey = boxes[check]
+            for i in getkey:
+                valid[i] = "unlocked"
+    #print(valid)
+    for validate in valid:
+        if validate == "locked":
+            return (False)
+    return (True)
---
+++ 0x01-lockboxes/check.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""check code"""
+canUnlockAll = __import__('0-lockboxes').canUnlockAll
+
+boxes = [[1], [2], [3], [4], []]
+boxes1 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+boxes2 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
+print(canUnlockAll(boxes1))
+print(canUnlockAll(boxes2))
