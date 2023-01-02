8c7b8ca2a349c00a1bf6c573cfd7af73c590e237
AJIyanu <aderemijoseph@live.com>
2023-01-02 11:24:33 +0100

    to validate

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,11 +11,15 @@ def canUnlockAll(boxes):
         valid.append("locked")
     valid[0] = "unlocked"
     for i in range(len(valid)):
+        print("working here", valid[i], i)
         for check in range(len(valid)):
+            print(check, "also working here")
             if valid[check] == "unlocked":
                 getkey = boxes[check]
+                print(getkey, "we are unlocking these boxes now")
             for i in getkey:
                 valid[i] = "unlocked"
+            print(valid)
     #print(valid)
     for validate in valid:
         if validate == "locked":
--- 0x01-lockboxes/check.py
+++ 0x01-lockboxes/check.py
@@ -5,6 +5,7 @@ canUnlockAll = __import__('0-lockboxes').canUnlockAll
 boxes = [[1], [2], [3], [4], []]
 boxes1 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 boxes2 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
-print(canUnlockAll(boxes1))
+#print(canUnlockAll(boxes))
+#print(canUnlockAll(boxes1))
+print(boxes2)
 print(canUnlockAll(boxes2))
