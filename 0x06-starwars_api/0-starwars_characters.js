ae8d6a921eeabc14c999fc76fe0350f37e2393d8
AJIyanu <aderemijoseph@live.com>
2023-03-08 03:35:33 -0800

    sync

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,7 +6,7 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + episode;
 console.log(url);
 
 request(url, function (error, response, body) {
-  console.log(error);
-  console.log(response);
-  console.log(body);
+  //console.log(error);
+  console.log(response.body);
+  //console.log(body);
 });
