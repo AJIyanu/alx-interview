a62cf7f1c7d0d4ee45fcf35f0bc2aeda3b7dcccc
AJIyanu <aderemijoseph@live.com>
2023-03-08 12:58:53 +0100

    sync

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,13 +1,25 @@
 #!/usr/bin/node
 
+const { rejects } = require('assert');
 const request = require('request');
 const episode = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + episode;
 console.log(url);
 
+async function makerequest (people_url) {
+  return new promise ((resolve, rejects) => {
+    request(people_url, function(error, response) {
+      console.log(error);
+      const result = JSON.parse(response.body);
+      console.log(result.name);
+    });
+  });
+}
+
 request(url, function (error, response, body) {
   console.log(error);
   const result = JSON.parse(response.body);
   console.log(result.characters);
-  //console.log(body);
+  const xter = result.characters;
+  makerequest(xter[1]);
 });
