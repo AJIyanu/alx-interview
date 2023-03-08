b3f250984d8bf329a756e258b926ed9035e7b6bc
AJIyanu <aderemijoseph@live.com>
2023-03-08 13:29:43 +0100

    check

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,27 +6,13 @@ const episode = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + episode;
 console.log(url);
 
-async function makerequest (people_url) {
-  return new Promise((resolve, reject) => {
-    request(people_url, function(error, response) {
-      console.log(error);
-      const result = JSON.parse(response.body);
-      console.log(result.name);
-    });
+async function logname (url) {
+  const peopleurl = await request(url, function (error, response) {
+    console.log(error);
+    return JSON.parse(response.body).characters;
   });
-}
-
-request(url, function (error, response, body) {
-  console.log(error);
-  const result = JSON.parse(response.body);
-  console.log(result.characters);
-  const xter = result.characters;
-  makerequest(xter[1]);
-});
-
-async function logname(url) {
-  const peopleurl = await request(url);
   console.log(typeof (peopleurl));
+  console.log(peopleurl);
 }
 
 logname(url);
