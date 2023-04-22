4b25723d3acd31eb097fc9ed8d2ab7355ffcf6c2
AJIyanu <aderemijoseph@live.com>
2023-04-22 10:05:32 +0100

    comment

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,7 +5,19 @@ Find the lowest combinations of number of coin
 
 
 def makeChange(coins, total: int) -> int:
-    """returns the lowest number of combination of coin"""
+    """
+    Returns the minimum number of coins needed to make up the total amount.
+
+    Args:
+        coins (list[int]): List of integers representing the denominations of
+        available coins.
+        total (int): The total amount of money for which the minimum number
+        of coins needs to be calculated.
+
+    Returns:
+        int: The minimum number of coins needed to make up the total amount,
+        or -1 if it is not possible.
+    """
     if total <= 0:
         return 0
     count: int=0
