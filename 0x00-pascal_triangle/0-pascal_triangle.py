58e6552de19c8a341df196a357e14d2eab9a33ad
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-09-27 16:16:12 +0100

    pascal traingle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""
+Function that returns a list of lists of integers
+representing the Pascal's triangle of n
+"""
+def pascal_triangle(n):
+    """
+    returns a list of intergers representing pascal triangle
+    returns empty list when n is zero
+    """
+    if n <= 0:
+        return []
+    pascal = []
+    for row in range(1, n + 1):
+        new_row = []
+        for post in range(row):
+            if post == row - 1:
+                new_row.append(1)
+            elif post == 0:
+                new_row.append(1)
+            else:
+                try:
+                    new_row.append(pascal[row - 2][post - 1] + pascal[row - 2][post])
+                except IndexError:
+                    pass
+        pascal.append(new_row)
+    return pascal
