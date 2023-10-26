ec5bb7944f884c33e8604f7ae582bb09aa019ca1
AJIyanu <aderemijoseph@live.com>
2023-10-26 22:15:52 +0100

    stnc

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,16 +7,14 @@ Utf-8 validation code
 def validUTF8(data):
     """returns true if valid false if not"""
     count = 0
-    if len(data) == 0:
-        return False
     for check in data:
         if count == 3 or count == 2 or count == 1:
             if check > 192 or check < 128:
                 return False
             count -= 1
         else:
-#            if check > 255:
-#                return False
+            if check > 255:
+                return False
             if check > 239:
                 count = 3
             elif check > 223:
