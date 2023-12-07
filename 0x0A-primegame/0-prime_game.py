1c2a4f2a9588ea5d2ea43535aed1a01580614d5f
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-12-07 22:01:46 +0100

    sync

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""prime game to eliminate prime numbers and multiples"""
+
+
+import sympy
+
+
+
+def isWinner(x, nums):
+    """find the most winner"""
+    pass
+
+if __name__ == "__main__":
+    numbers = []
+    for n in range(10):
+        numbers.append(n)
+    print(numbers)
