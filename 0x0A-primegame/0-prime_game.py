7f0d1bef39d4a5904e75a105b415d4745430f9c3
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-12-08 03:17:58 +0100

    prime game solved

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,16 +2,49 @@
 """prime game to eliminate prime numbers and multiples"""
 
 
-import sympy
-
-
 
 def isWinner(x, nums):
     """find the most winner"""
-    pass
+    if x == 0:
+        return None
+    ben = 0
+    maria = 0
+    for n in nums:
+        if roundWinnwer(n) == "Ben":
+            ben += 1
+        else:
+            maria += 1
+    if ben == maria:
+        return None
+    if ben > maria:
+        return "Ben"
+    return "Maria"
 
-if __name__ == "__main__":
-    numbers = []
-    for n in range(10):
-        numbers.append(n)
-    print(numbers)
+
+def generate_number_list(num):
+    """returns a list of number from 1 to including number"""
+    num_list = []
+    for n in range(1, num + 1):
+        num_list.append(n)
+    return num_list
+
+
+def remove_multiple(num_list, num):
+    """returns a new list with mulitples excluded"""
+    new = []
+    for n in num_list:
+        if n % num != 0:
+            new.append(n)
+    return new
+
+
+def roundWinnwer(rNumber):
+    """returns the winner for the number"""
+    count = 0
+    check_list = generate_number_list(rNumber)
+    while len(check_list) > 1:
+        check_list  = remove_multiple(check_list, check_list[1])
+        count += 1
+    if count % 2 == 0:
+        return "Ben"
+    return "Maria"
